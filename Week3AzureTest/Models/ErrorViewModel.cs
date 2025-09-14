43d8c838d97a3288665a6057342b4dff08e3e48f
Trubo <78948819+truubo@users.noreply.github.com>
2025-09-14 15:36:13 -0500

    Add project files.

---
+++ Week3AzureTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Week3AzureTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
